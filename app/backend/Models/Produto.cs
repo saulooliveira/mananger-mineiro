cf755cdcacd400620f56fb51045715fa33541b57
Saulo Oliveira <saulooliveira@outlook.com>
2026-04-28 14:18:24 -0300

    Task 3: Create Produto model

---
+++ app/backend/Models/Produto.cs
@@ -0,0 +1,12 @@
+namespace Backend.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public required string Codigo { get; set; }
+    public string? Yield { get; set; }
+    public required string Descricao { get; set; }
+    public string? Categoria { get; set; }
+    public decimal Valor { get; set; }
+    public int QuantidadeImpresa { get; set; } = 0;
+}
